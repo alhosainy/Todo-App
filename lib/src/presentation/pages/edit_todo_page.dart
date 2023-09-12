7a9c5a1a7831a8613a80cd5a40653c2b068cde61
alhosainy <alhosainyyaser@gmail.com>
2023-09-13 01:21:50 +0300

    removed logger from release

--- lib/src/presentation/pages/edit_todo_page.dart
+++ lib/src/presentation/pages/edit_todo_page.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shortid/shortid.dart';
@@ -22,7 +23,10 @@ class TodoEdit extends StatelessWidget {
     final controller = Get.put(EditTodoController(), permanent: true);
     final model = Get.find<ViewModelController>();
 
-    ViewModelController.logger.value.i('controllers updated with id: $todoId');
+    if (kDebugMode) {
+      ViewModelController.logger.value
+          .i('controllers updated with id: $todoId');
+    }
     if (todoId != null) {
       model.getTodo(todoId!).then((value) {
         if (value != null) {
